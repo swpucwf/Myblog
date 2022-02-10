b2381de9d8ca70b3a4d0ef95e6961663d83ac543
chenweifeng <1953981601@qq.com>
2022-02-10 11:47:20 +0800

    更新文档

---
+++ codes/c++/ex05/6-1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <cctype>
+using namespace std;
+// 编写一个程序，读取键盘输入，直到遇到`@`符号为止，并回显输入（数字除外），同时将大写字符转换为小写，将小写字符转换为大写（别忘了`cctype`函数系列）。
+int main() {
+	char input_word;
+	cout << "请输入你的字符：" << endl;
+	cin >> input_word;
+
+	while (input_word != '@') {
+
+		//        printf("1");
+		if (islower(input_word)) {
+			input_word = toupper(input_word);
+		}
+		else if (isupper(input_word))
+		{
+			input_word = tolower(input_word);
+		}
+		cout << input_word << endl;
+		cin >> input_word;
+	}
+	return 0;
+}
---
+++ codes/c++/ex05/6-2.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <array>
+#include <cctype>
+using namespace std;
+// 编写一个程序，最多将10个donation值读入到一个double数组中（如果您愿意，也可以使用模板类array）。
+// 程序遇到非数字输入时将结束输入，并报告这些数字的平均值以及数组中有多少个数字大于平均值。
+const int LENGTH = 10;
+int main() {
+	array<double, LENGTH> donation{};
+	int count = 0;
+	char number;
+	double sum = 0.;
+	double avg;
+	int new_count = 0;
+	cout << "请输入number的值:";
+	cin >> number;
+	while (isdigit(number) && count < LENGTH) {
+		cout << number;
+		donation[count] = (double)number - 48;
+		cout << donation[count] << endl;
+		count++;
+		cout << "请输入number的值:";
+		cin >> number;
+	}
+	for (int i = 0; i < donation.size(); ++i) {
+		sum += donation[i];
+	}
+	avg = sum / donation.size();
+
+	for (int i = 0; i < donation.size(); ++i) {
+		if (donation[i] > avg) {
+			new_count++;
+		}
+	}
+
+	cout << "all:" << new_count << " number are gather than " << avg << endl;
+	return 0;
+}
