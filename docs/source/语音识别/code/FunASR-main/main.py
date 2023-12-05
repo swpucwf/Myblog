4e78352abe16b534fb81fa0199f7c3ee8824a3f2
swpucwf <swpucwf@126.com>
2023-12-05 09:57:08 +0800

    yolov5 跟踪计数

--- docs/source/语音识别/code/FunASR-main/main.py
+++ docs/source/语音识别/code/FunASR-main/main.py
@@ -1,12 +1,6 @@
-
 from funasr import infer
 
 p = infer(model="paraformer-zh", vad_model="fsmn-vad", punc_model="ct-punc", model_hub="ms")
 
-
-
-
-
-for i in range(100):
-    res = p("./Rs_16k.wav", batch_size_token=1000)
-    print(res)
+res = p("Rs_16k.wav", batch_size_token=5000)
+print(res)
